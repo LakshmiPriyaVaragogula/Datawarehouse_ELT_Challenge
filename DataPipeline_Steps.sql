1f7ed1c0e94f3848fb976ca411929953b7aad1d2
LakshmiPriyaVaragogula <lakshmipriyavaragogula@gmail.com>
2025-03-26 00:10:29 +0530

    Add files via upload

---
+++ DataPipeline_Steps.sql
@@ -0,0 +1,19 @@
+Infer Schema from raw .dlm file.
+
+Auto-Load Data into Landing Tables from S3 via Snowpipe.
+
+Create Streams for incremental data tracking.
+
+Use Dynamic Tables to transform raw data.
+
+Normalize Hierarchies and establish fact-dimension relationships.
+
+Load into Fact & Dimension Tables with constraints.
+
+Implement Incremental Updates using Streams and Tasks.
+
+Create Materialized View for weekly sales aggregation.
+
+Define a Stored Procedure to update data.
+
+Schedule the Update Task every 5 minutes.
